0bbc5326818ab35eed79be657d9b4cf8000973db
Brett Stoudt <bstoudt01@gmail.com>
2020-08-21 14:25:08 -0500

    compelted exercise 1

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,6 +1,26 @@
 function main() {
   // Put your code here
 
+  const enemies = [
+    {
+      firstName: "Joshua",
+      lastName: "Flowers",
+      offenses: [
+        "Being a jerk to me in elementary school",
+        "Not being nice to me in elementary school"
+      ],
+      isReallyHated: true,
+    },
+  ];
+  console.log(enemies)
+  for (let enemy of enemies) {
+    console.log(enemy)
+    if (enemy.isReallyHated) {
+      console.log(`${enemy.firstName} ${enemy.lastName} Really Really dislike`)
+    }
+    else {
+      console.log(`${enemy.firstName} ${enemy.lastName}`)
+    }
+  };
 }
-
 main();
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,27 @@
 function main() {
-  // Put your code here
+    // Put your code here
+    const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+        "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+    //Print someout put to terminal
+    console.log("All Place Names");
+
+    //Iterate over each name in array of "names"
+    for (let name of names) {
+        console.log(name);
+    };
+
+    //blank line for spacing
+    console.log("")
+
+    const theNames = [];
+    for (let i = 0; i < names.length; i++) {
+        console.log("names[i]", names[i])
+        if (names[i].startsWith("The")) {
+            theNames.push(names[i])
+        }
+    }
+    console.log("theNames", theNames)
 
 }
 
